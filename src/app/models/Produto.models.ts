483528d63a82ce499f184a849be0ef3c41a93508
wilxavier <80928268+wilxavier@users.noreply.github.com>
2024-08-20 06:07:12 -0300

    insert services e spec

--- src/app/models/Produto.models.ts
+++ src/app/models/Produto.models.ts
@@ -5,5 +5,13 @@ export class Produto {
     foto: string = "";
     preco: number = 0;
 
-    
+    constructor(id:number, produto:string, descricao:string, foto: string, preco: number){
+        this.id = id;
+        this.produto = produto;
+        this.descricao = descricao;
+        this.foto = foto;
+        this.preco = preco;
+    }
+
 }
+
