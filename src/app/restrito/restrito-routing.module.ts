66b4be6cfd73d15124c28a6591dd01764c158df0
wilxavier <80928268+wilxavier@users.noreply.github.com>
2024-08-21 06:00:06 -0300

    subrota restritoModules

---
+++ src/app/restrito/restrito-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
+import { ListaProdutoComponent } from './lista-produto/lista-produto.component';
+import { AtualizaProdutoComponent } from './atualiza-produto/atualiza-produto.component';
+import { RestritoComponent } from './restrito.component';
+
+const restritoRoutes: Routes = [
+    { path: 'restrito', component: RestritoComponent, children:}
+]
+
+const routes: Routes = [
+  {
+    path:'restrito', component: RestritoComponent, children: [
+
+    { path: 'cadastro', component: CadastroProdutoComponent },
+    { path: 'lista', component: ListaProdutoComponent },
+    { path: 'editar/:id', component: AtualizaProdutoComponent },
+    ]
+  },
+  {path: '', redirectTo: '/restrito/lista', pathMatch: 'full'}
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(restritoRoutes)],
+    exports: [RouterModule]
+})
+export class RestritoRoutingModule {
+}
